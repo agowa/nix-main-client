47f62e502d562a8c886c4656306d30704b3da502
Klaus Frank <git@frank.fyi>
2026-06-26 00:21:08 +0200

    fix flake submodules issue

--- flake.nix
+++ flake.nix
@@ -6,10 +6,12 @@
     nixos-25-05.url = "path:./nixpkgs/nixos-25.05";
     nixos-25-11.url = "path:./nixpkgs/nixos-25.11";
     nixos-26-05.url = "path:./nixpkgs/nixos-26.05";
+    self.submodules = true;
+    self.lfs = true;
   };
 
   outputs = { self, nixpkgs, nixos-25-05, nixos-25-11, nixos-26-05, ... }: {
-    nixosConfigurations.pc-001 = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.PC-001 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
